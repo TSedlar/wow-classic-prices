1be154e9f8184cbbbe22aa78aa4ede7ca6a78f15
Tyler Sedlar <sedlarizona@gmail.com>
2020-10-01 20:11:07 -0700

    Fix Gold-Silver-Copper display

--- src/helper/WoWUtil.js
+++ src/helper/WoWUtil.js
@@ -11,8 +11,8 @@ module.exports.moneyToGSC = (m) => {
   if (m < 0) {
     return 'N/A'
   }
-  const gold = parseInt(Math.floor(m / 1000))
-  const silver = parseInt(Math.floor((m % 1000) / 100))
-  const copper = parseInt(Math.floor((m % 1000) % 100))
+  const gold = parseInt(Math.floor(m / 10000))
+  const silver = parseInt(Math.floor((m % 10000) / 100))
+  const copper = parseInt(Math.floor((m % 10000) % 100))
   return { gold, silver, copper, toString: () => `${gold}g ${silver}s ${copper}c` }
 }
